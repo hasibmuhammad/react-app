7e95eef10d3ea611aa968bae3693fb1f4ec05099
hasibmuhammad <hasibmu99@gmail.com>
2023-10-05 18:35:45 +0600

    New Form Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Form from "./components/Form";
+import NewForm from "./components/NewForm";
 
 const App = () => {
   return (
     <div>
-      <Form />
+      <NewForm />
     </div>
   );
 };
--- src/components/NewForm.tsx
+++ src/components/NewForm.tsx
@@ -4,16 +4,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
   name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age must be number" })
+    .min(18, { message: "Age should be at least 18" }),
   email: z.string().email({ message: "You must provide a valid email" }),
 });
 
 type FormData = z.infer<typeof schema>;
 
-const Form = () => {
+const NewForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => console.log(data);
@@ -21,8 +24,8 @@ const Form = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      action=""
       className="max-w-md mx-auto min-h-screen flex flex-col justify-center items-center px-10"
+      action=""
     >
       <div className="w-full mb-3">
         <label htmlFor="name">Name: </label>
@@ -36,6 +39,17 @@ const Form = () => {
         {errors.name && <p className="text-red-400">{errors.name.message}</p>}
       </div>
       <div className="w-full mb-3">
+        <label htmlFor="age">Age: </label>
+        <input
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          type="number"
+          placeholder="Enter your age"
+          className="px-6 py-4 border-2 border-orange-400 outline-none rounded-lg w-full"
+        />
+        {errors.age && <p className="text-red-400">{errors.age.message}</p>}
+      </div>
+      <div className="w-full mb-3">
         <label htmlFor="email">Email: </label>
         <input
           {...register("email")}
@@ -47,14 +61,25 @@ const Form = () => {
         {errors.email && <p className="text-red-400">{errors.email.message}</p>}
       </div>
 
-      <button
-        className="px-5 py-3 bg-orange-400 text-white font-bold rounded-lg"
-        type="submit"
-      >
-        Submit
-      </button>
+      {!isValid && (
+        <button
+          className="px-5 py-3 bg-orange-400 text-white font-bold rounded-lg cursor-not-allowed opacity-50"
+          type="submit"
+          disabled
+        >
+          Submit
+        </button>
+      )}
+      {isValid && (
+        <button
+          className="px-5 py-3 bg-orange-400 text-white font-bold rounded-lg"
+          type="submit"
+        >
+          Submit
+        </button>
+      )}
     </form>
   );
 };
 
-export default Form;
+export default NewForm;
